0ca27d69c139d46b194b115b8ce7eb8be952c1df
Yu Qin <zxas5820@yahoo.com.tw>
2020-01-01 19:20:43 +0800

    修改customer欄位phone轉為text

--- database/migrations/2019_12_29_134642_create_customers_table.php
+++ database/migrations/2019_12_29_134642_create_customers_table.php
@@ -17,7 +17,7 @@ class CreateCustomersTable extends Migration
             $table->bigIncrements('id');
             $table->integer('user_id');
             $table->text('name');
-            $table->integer('phone');
+            $table->text('phone');
             $table->text('idcard');
             $table->timestamps();
         });
