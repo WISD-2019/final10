fbe9028b54e08895d549feb0decfae50d22e326f
Yu Qin <zxas5820@yahoo.com.tw>
2020-01-05 16:50:16 +0800

    後端帳號管理路由

--- routes/web.php
+++ routes/web.php
@@ -41,8 +41,10 @@ Route::group(['prefix' => 'admin'], function() {
     Route::get('/posts/{id}/edit', ['as' => 'admin.posts.edit'  , 'uses' => 'AdminPostsController@edit']);
     Route::post('/posts',['as'=>'admin.posts.store','uses'=> 'AdminPostsController@store']);
     Route::patch('/posts/{id}',['as'=>'admin.posts.update','uses'=>'AdminPostsController@update']);
-    Route::delete('/posts/{id}',['as'=>'admin.posts.destroy','uses'=>'AdminPostsController@destroy']);
+    Route::delete('/posts/{id}',['as'=>'admin.posts.destroy','uses'=>'AdminPostsController@destroy']);    
 
     Route::get('/account'          , ['as' => 'admin.posts.account' , 'uses' => 'AccountController@index']);
-    Route::post('/account/create'  , ['as' => 'admin.posts.create' , 'uses' => 'AccountController@create']);
+    Route::post('/account/create'  , ['as' => 'admin.account.create' , 'uses' => 'AccountController@create']);
+    Route::post('/account/update'  , ['as' => 'admin.account.update' , 'uses' => 'AccountController@update']);
+    Route::post('/account/destroy'  , ['as' => 'admin.account.destroy' , 'uses' => 'AccountController@destroy']);
 });
