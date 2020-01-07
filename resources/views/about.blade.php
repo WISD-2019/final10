6f20e86b935a98804c61aed5fbcd78cda80ec1e0
Yu Qin <zxas5820@yahoo.com.tw>
2020-01-07 17:15:52 +0800

    前端訂單明細頁面部分修改

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -27,13 +27,34 @@
         <div class="row">
             <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
 
-                    金額:{{$total}}<p>
-                    是否付款:No<p>
-                    預約房間的房間號:{{$c}}號<p>
-                    房型:{{$room->type}}人房&nbsp;&nbsp;&nbsp;木質地板:{{$room->wood}}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;陽台:{{$room->balcony}}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    是否乾溼分離:{{$room->dry_wet}}<p>
-                    預定入住時間:{{$a}}<p>
-                    預定退房時間:{{$b}}<p>
+                    金額：{{$total}}<p>
+                    是否付款：尚未付款<p>
+                    預約房間的房間號： {{$c}} 號<p>
+                    房型： {{$room->type}} 人房
+                    &nbsp;&nbsp;&nbsp;
+                    木質地板：
+                    @if($room->wood=='Y' || $room->wood=='y')
+                        是 
+                    @else
+                        否 
+                    @endif
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    陽台：
+                    @if($room->balcony=='Y' || $room->balcony=='y')
+                        有 
+                    @else
+                        沒有 
+                    @endif                        
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    是否乾溼分離：
+                    @if($room->dry_wet=='Y' || $room->dry_wet=='y')
+                        是 
+                    @else
+                        否 
+                    @endif                    
+                    <p>
+                    預定入住時間：{{$a}}<p>
+                    預定退房時間：{{$b}}<p>
                     <form action="/go_home" method="GET">
                         {{ csrf_field() }}
                     <button type="submit" class="btn btn-danger">確定並回到首頁</button>
